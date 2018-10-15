c292fc371f6402deb80128dde2b0e9c7555c954f
lina02 <lina02missfresh.cn>
2018-10-15 10:56:59 +0800

    外观模式

---
+++ com/pattern/facade/FacadeClient.java
@@ -0,0 +1,16 @@
+package com.pattern.facade;
+
+/**
+ * 外观类客户端
+ * 由于Facade的作用，客户端可以根本不知道四个子系统的存在
+ * 启发：维护一个遗留的大型系统时，可能这个系统已经非常难以维护和扩展了，此时可以为新系统开发一个外观Facade类，
+ * 来提供设计粗糙或高度复杂的遗留代码的比较清晰简单的接口，让新系统与Facade对象交互，Facade与遗留代码交互所有复杂的工作
+ */
+public class FacadeClient {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+
+        facade.methodA();
+        facade.methodB();
+    }
+}
